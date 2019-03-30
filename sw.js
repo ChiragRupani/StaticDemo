86a980121c296b7d567f28e9ac5ccd7f6546c5fa
Chirag Rupani <36813124+chiragrupani@users.noreply.github.com>
2019-03-30 06:26:51 +0530

    InitialFiles

---
+++ sw.js
@@ -0,0 +1,67 @@
+﻿var CACHE_NAME = 'static_site_v2';
+var urlsToCache = [
+    '/',
+    '/favicon.ico',
+    '/css/site.css',
+    '/js/site.js',
+    '/lib/bootstrap/dist/css/bootstrap.min.css',
+    '/lib/jquery/dist/jquery.min.js',
+    '/lib/bootstrap/dist/js/bootstrap.bundle.min.js'
+];
+
+var cacheWhitelist = [CACHE_NAME];
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+
+                return fetch(event.request).then(
+                    function (response) {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+
+                        var responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(function (cache) {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
